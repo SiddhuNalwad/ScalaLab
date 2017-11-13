4b3f61e53a0e7040a729cf2e9b58650d1c1b9788
Nalwad, Siddhu <siddhu.nalwad@fmr.com>
2017-11-13 11:15:24 +0530

    -adding author

--- src/com/srigauri/scala/chapter10/ArrayElement.scala
+++ src/com/srigauri/scala/chapter10/ArrayElement.scala
@@ -1,5 +1,9 @@
 package com.srigauri.scala.chapter10
 
+/**
+ * @author Martin Odesky
+ * 
+ */
 class ArrayElement(conts: Array[String]) extends Element {
   def contents: Array[String] = conts 
 }
